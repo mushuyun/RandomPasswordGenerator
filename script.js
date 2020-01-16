2d83c6f835f776e06e5cec983047e1efc7097306
mushuyun <mushuyun@yahoo.com>
2020-01-16 16:13:53 -0500

    adding password generator files

---
+++ script.js
@@ -0,0 +1,223 @@
+var inputL = confirm ("Would you like to include lower case?");
+var inputU = confirm ("Would you like to include uppercase?");
+var inputN = confirm ("Would you like to include numbers?");
+var inputS = confirm ("Would you like to include symbols?");
+
+var length = prompt("Please enter password length from 8 to 128");
+var complexity = parseInt(length);
+
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+var number = "1234567890";
+var symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+if (complexity != null && inputL === true ){
+    let values = lower;
+    function generate(){ 
+        let password = "";
+        for(var i = 0; i < complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+            }
+            
+        document.getElementById("display").value = password;
+            
+        document.getElementById("lastNums").innerHTML += password + "<br />";         
+        }
+}
+
+if (complexity != null && inputU === true ){
+    let values = upper;
+    function generate(){ 
+        let password = "";
+        for(var i = 0; i < complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+            }
+            
+        document.getElementById("display").value = password;
+            
+        document.getElementById("lastNums").innerHTML += password + "<br />";         
+        }
+}
+
+if (complexity != null && inputN === true ){
+    let values = number;
+    function generate(){ 
+        let password = "";
+        for(var i = 0; i < complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+            }
+            
+        document.getElementById("display").value = password;
+            
+        document.getElementById("lastNums").innerHTML += password + "<br />";         
+        }
+}
+
+if (complexity != null && inputS === true ){
+    let values = symbol;
+    function generate(){ 
+        let password = "";
+        for(var i = 0; i < complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+            }
+            
+        document.getElementById("display").value = password;
+            
+        document.getElementById("lastNums").innerHTML += password + "<br />";         
+        }
+}
+
+if(complexity != null && inputL === true && inputU === true){
+
+    let values = lower.concat(upper);
+    
+    function generate(){ 
+        let password = "";
+        for(var i = 0; i < complexity; i++){
+         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+            }
+            
+        document.getElementById("display").value = password;
+            
+        document.getElementById("lastNums").innerHTML += password + "<br />";         
+        }  
+    }
+
+if(complexity != null && inputL === true && inputU === true && inputN === true){
+
+     let values = lower.concat(number, upper);
+        function generate(){ 
+            
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+                
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }   
+
+if(complexity != null && inputL === true && inputN === true){
+        
+    let values = lower.concat(number);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }      
+        
+if(complexity != null && inputU === true && inputN === true){
+        let values = upper.concat(number);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }  
+
+if(complexity != null && inputU === true && inputN === true && inputS === true){
+        let values = upper.concat(number, symbol);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }     
+
+if(complexity != null && inputU === true && inputS === true){
+        let values = upper.concat(symbol);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }  
+    
+if(complexity != null && inputN === true && inputS === true){
+        let values = number.concat(symbol);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }  
+
+if(complexity != null && inputL === true && inputS === true){
+        let values = lower.concat(symbol);
+        
+        function generate(){ 
+            let password = "";
+            for(var i = 0; i < complexity; i++){
+             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+                }
+                
+            document.getElementById("display").value = password;
+                
+            document.getElementById("lastNums").innerHTML += password + "<br />";         
+            }  
+    }  
+
+if(complexity !=null && inputL === true && inputU === true && inputN === true && inputS === true ) {
+    let values = lower.concat(upper, number, symbol);
+    
+    function generate(){
+    let password = "";
+    for(var i = 0; i < complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+         }
+
+    document.getElementById("display").value = password;
+
+    document.getElementById("lastNums").innerHTML += password + "<br />";
+
+    }
+} 
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+}
+
+if (complexity < 8 || complexity > 128 ){
+    alert("You have to enter a number between 8 and 128.");  
+    var length1 = prompt ("Please enter again");
+    complexity = parseInt(length1);
+}
